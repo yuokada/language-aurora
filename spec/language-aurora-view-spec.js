1bc3f3fb7a1ded4af26987d6f87ef4914e6fc01b
yuokada <callistoiv+git@gmail.com>
2017-10-25 01:22:35 +0900

    init

---
+++ spec/language-aurora-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LanguageAuroraView from '../lib/language-aurora-view';
+
+describe('LanguageAuroraView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
